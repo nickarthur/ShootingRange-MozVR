1555e873e1381c8bda4c6a42443b9f1d321597fe
Jason Würtz <jwuertz@upei.ca>
2015-01-24 18:57:29 -0400

    removed other tool detection

--- js/LeapScript.js
+++ js/LeapScript.js
@@ -28,13 +28,13 @@ Leap.loop({enableGestures: true},
             if (frame.hands.length > 0) {
                 var hand = frame.hands[0];
                 var nameMap = ["thumb", "index", "middle", "ring", "pinky"];
-                hand.pointables.forEach(function (pointable) {
-                    if (pointable.tool) {
-                        console.log("Tool");
-                        var dart = pointable;
-                    }//else
-                        //console.log(nameMap[pointable.type]);
-                });
+                //hand.pointables.forEach(function (pointable) {
+                //    if (pointable.tool) {
+                //        console.log("Tool");
+                //        var dart = pointable;
+                //    }//else
+                //        //console.log(nameMap[pointable.type]);
+                //});
                 hand.tools.forEach(function (tool) {
                     console.log.("Tool Detected.");
                 })
